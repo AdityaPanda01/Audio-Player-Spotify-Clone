ec084f5ec2499945bb947595e0fafb0c534a8c20
AdityaPanda01 <159115406+AdityaPanda01@users.noreply.github.com>
2024-05-16 21:41:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Starting javascript")
 async function song(){
-    let val=await fetch("https://github.com/AdityaPanda01/Audio-Player-Spotify-Clone/tree/main/songs")
+    let val=await fetch("https://adityapanda01.github.io/Audio-Player-Spotify-Clone/songs")
     let data = await val.text()
    // console.log(data)
 let box=document.createElement("div")
